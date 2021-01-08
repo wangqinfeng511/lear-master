d8564e019f35f55953c2cc41a1c84be4e71bdef2
wangqinfeng <wangqinfeng511@163.com>
2021-01-08 11:03:07 +0800

    zhengli

--- 运维笔记/脚本/backupdb.sh
+++
@@ -1,9 +0,0 @@
-#echo 'begin'
-
-tablesname="`mysql -ujianqu -pletmego -h1.4.35.170 -P1025 -e "use JianQuStest;show tables;" | grep -v logs | grep -v Tables_in`"
-
-#echo ${tablesname}
-
-mysqldump -ujianqu -pletmego -h1.4.35.170 -P1025 JianQuStest `echo ${tablesname}` |gzip > /tmp/DBTest.sql.gz 
-
-#echo 'end'
